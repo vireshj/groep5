563c6e973bf72d617d5f5fc3c037fb4c2264c07d
Viresh Jagesser <viresh.jagesser@gmail.com>
2011-11-19 20:00:22 +0100

    controller comments toegevoegd

--- code/src/controller.java
+++ code/src/controller.java
@@ -6,9 +6,11 @@ public class controller {
 	}
 	// vind in de database een matching liedje.
 	public static void findSong(Lied invoer){
-		
+		//hier worden methodes aangeroepen om de ingevoerde liedje te vinden in de db
 	}
+	//hier worden liedjes gevonden die de gebruiker mogelijk ook leuk zal vinden 
 	public static void findSimilarSongs(Lied invoer){
-		
+		//methodes anroepen die gelijksoortige liedjes kan vinden op basis van de invoer
+		//arraylist vullen van de gui
 	}
 }
