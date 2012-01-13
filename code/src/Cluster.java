13673c33254ff77fbf70e895f2777a6d6c4a8392
Kaj <kaj_oudshoorn@hotmail.com>
2012-01-13 17:46:33 +0100

    ..

--- code/src/Cluster.java
+++ code/src/Cluster.java
@@ -1,6 +1,7 @@
 import java.util.*;
 
 
+
 public class Cluster {
 	Collection<String> tags;
 	 ArrayList<Lied> songs;
@@ -11,22 +12,18 @@ public class Cluster {
 		this.songs = songs;
 	}
 	
-	//methode die een lijst van liedjes omzet naar een lijst van n liedjes met de meeste overlap van tags
 	public ArrayList<Lied> cluster(ArrayList<Lied> liedjes , int n)
 	{
-		//een arraylist voor elk mogelijk aantal van overlap: 0 of 1 of ... of tags.size()
 		ArrayList<ArrayList<Lied>> clusters = new ArrayList<ArrayList<Lied>>();
 		for(int k = 0; k <= tags.size(); k++)
 		{
 			clusters.add(new ArrayList<Lied>());
 		}
-		
 		Iterator<String> tagI;
 		int count;
 		ArrayList<Lied> cluster;
 		Collection<String> tagNames;
 		Lied liedje;
-		//elk liedje afgaan
 		for(int i = 0; i < liedjes.size(); i++)
 		{
 			tagI = tags.iterator();
@@ -34,7 +31,6 @@ public class Cluster {
 			liedje = liedjes.get(i);
 			tagNames = liedje.getTag();
 			String ss;
-			//voor elke tag van het liedje controleren hoeveel er gelijk zijn
 			while(tagI.hasNext())
 			{
 				ss = tagI.next();
@@ -43,7 +39,6 @@ public class Cluster {
 					count++;
 				}
 			}
-			//toevoegen aan de bij de count horende lijst
 			clusters.get(count).add(liedje);
 		}
 		ArrayList<Lied> result = new ArrayList<Lied>();
@@ -54,8 +49,6 @@ public class Cluster {
 		ArrayList<Integer> amount = new ArrayList<Integer>();
 		int artiestIndex;
 		Lied currentSong;
-		//resultaat verkrijgen door liedjes uit de countlijsten toe te voegen totdat er genoeg liedjes in het resultaat staan
-		//of tot er geen liedjes meer zijn. Liedjes uit de lijsten met meeste counts worden als eerste gekozen.
 		while(result.size() < n && l >= 0)
 		{
 			cluster = clusters.get(l);
@@ -63,13 +56,9 @@ public class Cluster {
 			while(m < cluster.size() && result.size() < n)
 			{				
 				currentSong = cluster.get(m);
-				//liedjes mogen niet gekozen worden als de gebruiker ze al heeft (songs) 
-				//en mogen niet dubbel in het resultaat voorkomen (result)
 				if(!songs.contains(currentSong) && !result.contains(currentSong))
 				{
 					artiestIndex = artiesten.indexOf(currentSong.getArtiest());
-					//een artiest mag maximaal 2x voorkomen in een recommendatie
-					//en de count van een artiest wordt bijgehouden
 					if(artiestIndex >= 0 && amount.get(artiestIndex) < 2)
 					{
 						result.add(currentSong);
--- code/src/controller.java
+++ code/src/controller.java
@@ -1,113 +1,109 @@
-	import java.util.ArrayList;
-	import java.util.Collection;
-	import java.util.Iterator;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+
+import de.umass.lastfm.Tag;
+
+
+public class controller {
+	private static final String apiKey = "9b24762f4ec58fed178083b4d9f255c2";
 	
-	import de.umass.lastfm.Tag;
-	
-	
-	public class controller {
-		private static final String apiKey = "9b24762f4ec58fed178083b4d9f255c2";
-	
-		//hier wordt de gui aangemaakt
-		public static void main(String [] args){
-			new gui();
-		}
-		// vind in de database een matching liedje.
-		public static Lied findSong(Lied invoer)
-		{
-			Lied gevonden;
-			try {
-				gevonden = lastFM.searchSong(invoer.getNaam(), invoer.getArtiest());
-			}
-			catch(Exception e){return null;}
-			return gevonden;
-		}
-	
-		//recommendeer methode die gebruik maakt van de methode van lastFM
-		//gebruikt in eerste iteratie
-		public static void findSimilarSongs(Lied invoer){
-			//methodes anroepen die gelijksoortige liedjes kan vinden op basis van de invoer
-			//arraylist vullen van de gui setFeedbackPanel(Lied invoer)
-			ArrayList<Lied> songs = lastFM.similarSongs(invoer);
-			gui.setFeedbackPanel(songs);
-	
-		}
-	
-		//recommendeer methode die gebruik maakt van klasse Cluster om de best overeenkomende liedjes te geven  i.v.m. de gegeven lijst
-		//huidige recommendeer methode
-		public static void findSimilarSongsCluster(Collection<String> tags, ArrayList<Lied> song)
-		{
-			ArrayList<Lied> liedjes = new ArrayList<Lied>();
-			ArrayList<Lied> tagLiedjes;	
-	
-			for(String tagName : tags)
-			{
-				Iterator<Tag> i = Tag.search(tagName, apiKey).iterator();
-				Tag tag = i.next();
-				while(!tag.getName().equals(tagName) && i.hasNext())
-				{
-					Tag dummy = i.next();
-					if(dummy.getName().equals(tagName))
-						tag = dummy;
-				}
-				try{
-					tagLiedjes = lastFM.getTopTracks(tag);
-				} catch(Exception e){
-					System.out.println(tag.getName());
-					tagLiedjes = new ArrayList<Lied>();
-				}
-				for(Lied liedje : tagLiedjes)
-				{				
-					liedjes.add(liedje);
-				}
-			}
-			Cluster c = new Cluster(tags, song);
-			ArrayList<Lied> result = c.cluster(liedjes, 10);
-			gui.setFeedbackPanel(result);
-		}
-	
-		//methode om te klassificeren m.b.v. een decision tree.
-		//wordt niet meer gebruikt
-		public static void findSimilairSongsDecisionTree(Collection<String> tags, Lied song){
-			ArrayList<Lied> liedjes = new ArrayList<Lied>();
-			ArrayList<Lied> tagLiedjes;	
-	
-			for(String tagName : tags)
-			{
-				Iterator<Tag> i = Tag.search(tagName, apiKey).iterator();
-				Tag tag = i.next();
-				while(!tag.getName().equals(tagName) && i.hasNext())
-				{
-					Tag dummy = i.next();
-					if(dummy.getName().equals(tagName))
-						tag = dummy;
-				}
-				try{
-					tagLiedjes = lastFM.getTopTracks(tag);
-				} catch(Exception e){
-					System.out.println(tag.getName());
-					tagLiedjes = new ArrayList<Lied>();
-				}
-				for(Lied liedje : tagLiedjes)
-				{				
-					liedjes.add(liedje);
-				}
-			}
-	
-			//opbouw v/d decision tree
-			Iterator<String> i = tags.iterator();
-			DTreeNode prev = new DTreeNode(i.next(), song);
-			DTreeNode next;
-			DTreeNode root = new DTreeNode(i.next(), prev, song);;
-			while(i.hasNext())
-			{
-				next = root;
-				root = new DTreeNode(i.next(), prev,  song);
-				prev = next;
-			}
-	
-			//vind 10 meest vergelijkbare liedjes
-			ArrayList<Lied> result = root.classify(liedjes, 10);
-			gui.setFeedbackPanel(result);		
-		}
+	//hier wordt de gui aangemaakt
+	public static void main(String [] args){
+		new gui();
 	}
+	// vind in de database een matching liedje.
+	public static Lied findSong(Lied invoer)
+	{
+	//hier worden methodes aangeroepen om de ingevoerde liedje te vinden in de db
+		
+		Lied gevonden;
+		try {
+			gevonden = lastFM.searchSong(invoer.getNaam(), invoer.getArtiest());
+		}
+		catch(Exception e){return null;}
+		return gevonden;
+	}
+	
+	//hier worden liedjes gevonden die de gebruiker mogelijk ook leuk zal vinden 
+	public static void findSimilarSongs(Lied invoer){
+		//methodes anroepen die gelijksoortige liedjes kan vinden op basis van de invoer
+		//arraylist vullen van de gui setFeedbackPanel(Lied invoer)
+		ArrayList<Lied> songs = lastFM.similarSongs(invoer);
+		gui.setFeedbackPanel(songs);
+		
+	}
+	
+	public static void findSimilarSongsCluster(Collection<String> tags, ArrayList<Lied> song)
+	{
+		ArrayList<Lied> liedjes = new ArrayList<Lied>();
+		ArrayList<Lied> tagLiedjes;	
+		
+		for(String tagName : tags)
+		{
+			Iterator<Tag> i = Tag.search(tagName, apiKey).iterator();
+			Tag tag = i.next();
+			while(!tag.getName().equals(tagName) && i.hasNext())
+			{
+				Tag dummy = i.next();
+				if(dummy.getName().equals(tagName))
+					tag = dummy;
+			}
+			try{
+				tagLiedjes = lastFM.getTopTracks(tag);
+			} catch(Exception e){
+				System.out.println(tag.getName());
+				tagLiedjes = new ArrayList<Lied>();
+			}
+			for(Lied liedje : tagLiedjes)
+			{				
+				liedjes.add(liedje);
+			}
+		}
+		Cluster c = new Cluster(tags, song);
+		ArrayList<Lied> result = c.cluster(liedjes, 10);
+		gui.setFeedbackPanel(result);
+	}
+		public static void findSimilairSongsDecisionTree(Collection<String> tags, Lied song){
+		ArrayList<Lied> liedjes = new ArrayList<Lied>();
+		ArrayList<Lied> tagLiedjes;	
+		
+		for(String tagName : tags)
+		{
+			Iterator<Tag> i = Tag.search(tagName, apiKey).iterator();
+			Tag tag = i.next();
+			while(!tag.getName().equals(tagName) && i.hasNext())
+			{
+				Tag dummy = i.next();
+				if(dummy.getName().equals(tagName))
+					tag = dummy;
+			}
+			try{
+				tagLiedjes = lastFM.getTopTracks(tag);
+			} catch(Exception e){
+				System.out.println(tag.getName());
+				tagLiedjes = new ArrayList<Lied>();
+			}
+			for(Lied liedje : tagLiedjes)
+			{				
+				liedjes.add(liedje);
+			}
+		}
+				
+		//opbouw v/d decision tree
+		Iterator<String> i = tags.iterator();
+		DTreeNode prev = new DTreeNode(i.next(), song);
+		DTreeNode next;
+		DTreeNode root = new DTreeNode(i.next(), prev, song);;
+		while(i.hasNext())
+		{
+			next = root;
+			root = new DTreeNode(i.next(), prev,  song);
+			prev = next;
+		}
+		
+		//vind 10 meest vergelijkbare liedjes
+		ArrayList<Lied> result = root.classify(liedjes, 10);
+		gui.setFeedbackPanel(result);		
+	}
+}
