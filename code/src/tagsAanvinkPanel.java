78f30d163d634361cd6641c7132e1e7983b8c35e
Salim Hadri <salim.hadri@gmail.com>
2012-01-13 13:55:53 +0100

    Vrash rox the double

--- code/src/tagsAanvinkPanel.java
+++ code/src/tagsAanvinkPanel.java
@@ -14,12 +14,22 @@ public class tagsAanvinkPanel extends JPanel{
 	//hier wordt een tag toegevoegd aan de arraylist
 	public void add(Collection<String> tag){
 		Iterator<String> x = tag.iterator();
+		boolean used = false;
 		while(x.hasNext()){
+			used = false;
 			String tijdelijk = x.next();
-			tagCheckBox tagcheck = new tagCheckBox(new JCheckBox(tijdelijk,true),tijdelijk);
-			tags.add(tagcheck);
-			this.add(tagcheck.getJCheckBox());
-		//	System.out.println(tagcheck.getTag());
+			for(tagCheckBox dubbel : tags){
+				if(dubbel.getTag().equals(tijdelijk)){
+					used = true;
+				}
+					
+			}
+			if(!used){
+				tagCheckBox tagcheck = new tagCheckBox(new JCheckBox(tijdelijk,true),tijdelijk);
+				tags.add(tagcheck);
+				this.add(tagcheck.getJCheckBox());
+			}
+			
 		}
 	}
 	public ArrayList<String> getCheckedPanels(){
